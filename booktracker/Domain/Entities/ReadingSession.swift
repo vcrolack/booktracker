8c5fad8989da5348446d01eca6e652217de439d9
Victor Rolack <victorolack@gmail.com>
2026-02-17 14:46:55 -0300

    feat(domain): add reading session entity

---
+++ booktracker/Domain/Entities/ReadingSession.swift
@@ -0,0 +1,56 @@
+//
+//  ReadingSession.swift
+//  booktracker
+//
+//  Created by Victor rolack on 17-02-26.
+//
+
+import Foundation
+
+enum ReadingSessionDomainError: Error, Equatable {
+    case invalidDateRange
+    case invalidPageProgression
+}
+
+struct ReadingSession: Identifiable, Codable, Equatable {
+    let id: UUID
+    let bookId: UUID
+    
+    private(set) var startTime: Date
+    private(set) var endTime: Date
+    private(set) var startPage: Int?
+    private(set) var endPage: Int
+    
+    var durationInSeconds: TimeInterval {
+        return endTime.timeIntervalSince(startTime)
+    }
+    
+    init(id: UUID = UUID(), bookId: UUID, startTime: Date, endTime: Date, startPage: Int? = nil, endPage: Int) {
+            self.id = id
+            self.bookId = bookId
+            self.startTime = startTime
+            self.endTime = endTime
+            self.startPage = startPage
+            self.endPage = endPage
+        }
+    
+    mutating func updateSession(newStartTime: Date? = nil, newEndTime: Date? = nil, newEndPage: Int? = nil) throws {
+        let tentativeStart = newStartTime ?? self.startTime
+        let tentativeEnd = newEndTime ?? self.endTime
+        
+        guard tentativeEnd >= tentativeStart else {
+            throw ReadingSessionDomainError.invalidDateRange
+        }
+        
+        if let startP = self.startPage, let tentativeEndPage = newEndPage {
+            guard tentativeEndPage >= startP else {
+                throw ReadingSessionDomainError.invalidPageProgression
+            }
+        }
+        
+        if let start = newStartTime { self.startTime = start }
+        if let end = newEndTime { self.endTime = end }
+        if let page = newEndPage { self.endPage = page }
+    }
+}
+
